670a64dd458ba2e320130a9de1f0f75df4cfffb9
MilanAlgama <milanharsha28@gmail.com>
2026-06-28 22:50:40 +0530

    Configure Tailwind CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
 function App() {
   return (
-    <>
-      <h1>Hello Milan </h1>
-    </>
+    <div className="min-h-screen bg-[#050816] flex items-center justify-center">
+      <div className="text-center">
+        <h1 className="text-6xl font-bold text-blue-500">
+          Milan Harsha
+        </h1>
+
+        <p className="text-gray-300 mt-4 text-xl">
+          ICT Undergraduate | Cloud & DevOps Enthusiast
+        </p>
+
+        <button className="mt-8 bg-blue-600 hover:bg-blue-700 px-6 py-3 rounded-lg transition duration-300">
+          Portfolio
+        </button>
+      </div>
+    </div>
   );
 }
 
